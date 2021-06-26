5668ed5bd191c7561dd1577e700def157fd1cd01
k <gmateust@gmail.com>
2021-06-26 19:58:17 -0300

    Create Navigation bar and config styles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+
+import useStyles from './styles';
+
+const Navbar = ({ totalCart }) => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+                        <img src="./logo512.png" alt="commerce.js" height="25px" className={classes.image} />
+                        Loja
+                    </Typography>
+                    <div className={classes.grow} />
+                    {location.pathname === '/' && (
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalCart.length} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div> )}
+                </Toolbar>
+            </AppBar>
+        </  >
+    )
+};
+
+export default Navbar;
